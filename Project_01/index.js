07e55119deea072ffb3913fa8f67224308ee8b83
Siddhant2002 <siddhantswarupmallick@gmail.com>
2024-12-03 20:17:57 +0530

    Files added

--- Project_01/index.js
+++ Project_01/index.js
@@ -1,6 +1,37 @@
 const express = require("express");
+const users = require("./document.json");
 
 const app = express();
+
+// Route
+
+app.get("/users", (req, res) => {
+    const html = 
+    `
+    <ul>
+        ${users.map((user) => `<li>${user.first_name}</li>`).join("")};
+    </ul>
+    `
+    res.send(html);
+});
+
+app.get("/api/users", (req, res) => {
+    res.json(users);
+});
+
+app.get("/api/users/:id", (req,res) => {
+    const id = Number(req.params.id);
+    const user = users.find(user => user.id === id)
+    res.json(user);
+});
+
+app.patch("/api/users/:id", (req, res) => {
+    return res.json({status: "Pending"});
+})
+
+app.delete("/api/users/:id", (req, res) => {
+    return res.json({status: "Pending"});
+})
 
 const port = 8000;
 
