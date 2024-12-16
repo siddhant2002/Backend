dff310da4cd6705e72d849b6dac49bb9383a7ef9
Siddhant2002 <siddhantswarupmallick@gmail.com>
2024-12-17 00:04:48 +0530

    Files added

--- Project_02/service/auth.js
+++ Project_02/service/auth.js
@@ -1,13 +1,39 @@
-const sessionIdToUserMap = new Map();
+// const sessionIdToUserMap = new Map();
 
-function setUser(id, user)
+const jwt = require("jsonwebtoken");
+const secret = "Siddhant@203223";
+
+// function setUser(id, user)
+// {
+//     sessionIdToUserMap.set(id, user);
+// }
+
+function setUser(user)
 {
-    sessionIdToUserMap.set(id, user);
+    return jwt.sign(
+        {
+            _id: user._id,
+            email: user.email,
+        }, secret);
 }
 
-function getUser(id)
+// function getUser(id)
+// {
+//     return sessionIdToUserMap.get(id);
+// }
+
+function getUser(token)
 {
-    return sessionIdToUserMap.get(id);
+    if(!token)
+    {
+        return null;
+    }
+    try {
+        return jwt.verify(token, secret);
+    } catch (error) {
+        return null;
+    }
+    
 }
 
 module.exports = {
